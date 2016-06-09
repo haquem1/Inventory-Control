8bc4bd685415e7a1506b0f33b3f69b4ced4a6525
gab3alm <gab.alm94@gmail.com>
2016-06-09 11:50:18 -0700

    Update db_connect.php

--- scripts/db_connect.php
+++ scripts/db_connect.php
@@ -6,12 +6,6 @@ $username = "root";
 $password = "";
 $dbname = "vidteam_inventory";
 
-// ANA's Server
-// $servername = "lemonaide.ampenaranda.com";
-// $username = "lemonvidteam";
-// $password = "Gainz_keytosuccess!";
-// $dbname = "vidteam_inventory";
-
     // create connection
 $connection = new mysqli($servername, $username, $password, $dbname);
 
